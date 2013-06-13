12b72b8f883d8c1a1dfad4694e170cdeb25ebcff
Tietokone <wtl4112@rit.edu>
2013-06-13 13:52:29 -0400

    Some helper functions (CPUID, READMSR)
    This are needed to setup the processor's APIC for the external interrupts

---
+++ kernel/src/arch/x86_64/cpuid.h
@@ -0,0 +1,25 @@
+#ifndef __X86_64_CPUID_H__
+#define __X86_64_CPUID_H__
+
+#include "inttypes.h"
+
+// Adapted from: http://wiki.osdev.org/CPUID
+
+static inline
+void cpuid(uint32_t code, uint32_t* eax, uint32_t* edx)
+{
+	__asm__ volatile ("cpuid" : 
+					  "=a"(*eax), "=d"(*edx) : // Outputs
+					  "a"(code) : // Inputs
+					  "ecx","ebx");	// Clobbered registers
+}
+
+static inline
+void readmsr(uint32_t msr_reg, uint32_t* eax, uint32_t* edx)
+{
+	__asm__ volatile ("rdmsr" :
+						"=a"(*eax), "=d"(*edx) : // Outputs
+						"c"(msr_reg) :); // Inputs
+}
+
+#endif
