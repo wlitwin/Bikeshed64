1eb6164a716db2a3bca5f9939ca36eebcf51c530
Tietokone <wtl4112@rit.edu>
2013-06-16 23:14:40 -0400

    Added some comments

--- kernel/src/kernel/interrupts/defs.h
+++ kernel/src/kernel/interrupts/defs.h
@@ -1,6 +1,17 @@
 #ifndef __INTERRUPT_DEFS_H__
 #define __INTERRUPT_DEFS_H__
 
+#include "inttypes.h"
+
+typedef void (*interrupt_handler)(uint64_t vector, uint64_t error_code);
+
+/* Initialize the interrupt sub-system. This function is defined in
+ * the architecture specific files.
+ */
 extern void interrupts_init(void);
+
+/* Install an interrupt handler at the specified vector number.
+ */
+extern void interrupts_install_isr(uint64_t vector, interrupt_handler handler);
 
 #endif
