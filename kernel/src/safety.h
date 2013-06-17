1ee84f2f95c5f20b581d90e2775a71939e12c6ad
Tietokone <wtl4112@rit.edu>
2013-06-16 23:08:45 -0400

    Added a runtime assert macro

--- kernel/src/safety.h
+++ kernel/src/safety.h
@@ -1,8 +1,17 @@
 #ifndef __SAFETY_H__
 #define __SAFETY_H__
 
+#include "kernel/panic.h"
+
 // Credit to:
 // http://blogs.msdn.com/b/abhinaba/archive/2008/10/27/c-c-compile-time-asserts.aspx
 #define COMPILE_ASSERT(x) extern int __dummy[ (int) (x)!=0 ]
+
+// http://stackoverflow.com/questions/5641427/how-to-make-preprocessor-generate-a-string-for-line-keyword
+#define S(X) #X
+#define S_(X) S(X)
+#define S__LINE__ S_(__LINE__)
+
+#define ASSERT(X) if (!(X)) panic("ASSERT FAILED: " __FILE__ ":" S__LINE__)
 
 #endif
