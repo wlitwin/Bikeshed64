1bf4ea57e75e5940c8fa9237118d5ac2db0ed3f9
Tietokone <wtl4112@rit.edu>
2013-06-16 23:11:13 -0400

    Added calls to the scheduler and alloc functions

--- kernel/src/kernel/kmain.c
+++ kernel/src/kernel/kmain.c
@@ -1,13 +1,23 @@
 #include "kernel/virt_memory/defs.h"
 #include "kernel/interrupts/defs.h"
+#include "kernel/scheduler/scheduler.h"
+#include "kernel/alloc/alloc.h"
+
+#include "safety.h"
 
 void kmain(void)
 {
 	/* Initialize the memory sub-system */
 	virt_memory_init();
 
+	/* Initialize the kernel's memory allocators */
+	alloc_init();
+
 	/* Initialize the interupt sub-system */
 	interrupts_init();
+
+	/* Initialize the scheduler */
+	scheduler_init();
 
 	while (1) {
 		__asm__("hlt");
