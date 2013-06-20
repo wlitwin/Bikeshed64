5330a8e3818b2b479031e6276800bb7fb893ca12
Tietokone <wtl4112@rit.edu>
2013-06-20 13:24:24 -0400

    Added virt_reset_table() declaration

--- kernel/src/kernel/virt_memory/defs.h
+++ kernel/src/kernel/virt_memory/defs.h
@@ -3,20 +3,9 @@
 
 #include "inttypes.h"
 
-// TODO - Include these based on architecture type
-#define PG_FLAG_RW 0x2
-#define PG_FLAG_USER 0x4
-#define PG_FLAG_PWT 0x8
-#define PG_FLAG_PCD 0x10
-#define PG_FLAG_XD 0x8000000000000000
-
-#define PAGE_SMALL 0x1
-#define PAGE_LARGE 0x2
-
-#define PAGE_SMALL_SIZE 0x1000
-#define PAGE_LARGE_SIZE 0x200000
-
-extern uint64_t kernel_PML4;
+#ifdef BIKESHED_X86_64
+#include "arch/x86_64/virt_memory/imports.h"
+#endif
 
 extern void virt_memory_init(void);
 
@@ -28,6 +17,8 @@ extern uint8_t virt_map_page(void* table, const uint64_t virt_addr,
 						const uint64_t flags, const uint64_t page_size);
 
 extern void virt_unmap_page(void* table, uint64_t virt_addr);
+
+extern void virt_reset_table(void* table);
 
 extern void virt_cleanup_table(void* table);
 
