d5791f62e2cd066ecced35cfa74907d89bd291d2
Tietokone <wtl4112@rit.edu>
2013-06-20 20:21:32 -0400

    Added calls to initialize other modules

--- kernel/src/kernel/kmain.c
+++ kernel/src/kernel/kmain.c
@@ -1,7 +1,10 @@
 #include "kernel/virt_memory/defs.h"
 #include "kernel/interrupts/defs.h"
+#include "kernel/timer/timer.h"
 #include "kernel/scheduler/scheduler.h"
 #include "kernel/alloc/alloc.h"
+#include "kernel/elf/elf.h"
+#include "kernel/kprintf.h"
 
 void kmain(void)
 {
@@ -17,6 +20,13 @@ void kmain(void)
 	/* Initialize the scheduler */
 	scheduler_init();
 
+	/* Initialize the timer */
+	timer_init();
+
+	/* Setup the init process */
+	create_init_process();
+
+	/* Wait forever, should not happen */
 	while (1) {
 		__asm__("hlt");
 	}
