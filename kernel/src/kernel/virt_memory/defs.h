6c8f30f5fab53d765fbdbf14faaffef0b05cf079
Tietokone <wtl4112@rit.edu>
2013-06-24 20:32:05 -0400

    Added new definitions to the virtual memory import header
    -Added virt_lookup_phys
    -Added virt_clone_mapping
    -Added extern void* kernel_table

--- kernel/src/kernel/virt_memory/defs.h
+++ kernel/src/kernel/virt_memory/defs.h
@@ -7,6 +7,8 @@
 #include "arch/x86_64/virt_memory/imports.h"
 #endif
 
+extern void* kernel_table;
+
 extern void virt_memory_init(void);
 
 extern uint8_t virt_map_phys(void* table, const uint64_t virt_addr, 
@@ -17,12 +19,14 @@ extern uint8_t virt_map_page(void* table, const uint64_t virt_addr,
 						const uint64_t flags, const uint64_t page_size,
 						uint64_t* phys_addr);
 
+extern uint8_t virt_lookup_phys(void* table, uint64_t virt_addr, uint64_t* out_phys);
+
 extern void virt_unmap_page(void* table, uint64_t virt_addr);
 
 extern void virt_reset_table(void* table);
 
 extern void virt_cleanup_table(void* table);
 
-extern void* virt_clone_mapping(const void* table);
+extern void* virt_clone_mapping(void* table);
 
 #endif
