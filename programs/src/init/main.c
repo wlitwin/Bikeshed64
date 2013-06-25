ad598646895375d1e209b02d952e6c6eb4518c02
Tietokone <wtl4112@rit.edu>
2013-06-24 20:38:27 -0400

    Quick program to test the fork system call

--- programs/src/init/main.c
+++ programs/src/init/main.c
@@ -1,4 +1,5 @@
 #include "../inttypes.h"
+#include "../ulib.h"
 
 #define VIDEO_ADDRESS 0xFFFF8000000B8000
 #define MAX_X 80
@@ -13,6 +14,8 @@ typedef struct
 video_cell* video = (video_cell*)VIDEO_ADDRESS;
 uint16_t x;
 uint16_t y;
+
+uint8_t color = 0x9;
 
 void clear_screen()
 {
@@ -39,17 +42,17 @@ void text_mode_char(const char c)
 	}
 
 	video[y*80+x].c = c;
-	video[y*80+x].color = 0x9;
+	video[y*80+x].color = color;
 
 	++x;
 	if (x >= MAX_X)
 	{
 		x = 0;
-	}
-	++y;
-	if (y >= MAX_Y)
-	{
-		y = 0;
+		++y;
+		if (y >= MAX_Y)
+		{
+			y = 0;
+		}
 	}
 }
 
@@ -64,9 +67,43 @@ void text_mode_string(const char* str)
 
 void main(void)
 {
+	Pid pid;
+	if (fork(&pid) == SUCCESS)
+	{
+		const char* str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1";
+		int count = 0;
+		if (pid == 0)
+		{
+			// parent
+			while(1)
+			{
+				++count;
+				const int index = (count+x) % 27;
+				char printit[2] = { str[index], '\0' };
+				text_mode_string(printit);
+				__asm__ volatile("hlt");
+			}
+		}
+		else
+		{
+			color = 0xC;
+			++y;
+			// child
+			while(1)
+			{
+				++count;
+				const int index = (count+x) % 27;
+				char printit[2] = { str[index], '\0' };
+				text_mode_string(printit);
+				__asm__ volatile("hlt");
+			}
+			return;
+		}
+	}
+
 	x = y = 0;
 	while (1)
 	{
-		text_mode_string("Hello World!");
+		text_mode_string("Hello World! ");
 	}
 }
