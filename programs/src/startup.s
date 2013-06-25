6650ed83a863b090dd1bf402da844bb9a04095fe
Tietokone <wtl4112@rit.edu>
2013-06-24 20:07:43 -0400

    Added some code to clear the BSS

--- programs/src/startup.s
+++ programs/src/startup.s
@@ -3,6 +3,15 @@
 
 .globl _start
 _start:
+	# Clear the BSS section
+	movabsq $sbss, %rdi
+	movabsq $ebss, %rax
+clear_bss_loop:
+	movq $0, (%rdi)
+	addq $8, %rdi
+	cmpq %rax, %rdi
+	jb clear_bss_loop
+
 	call main
 
 	call exit
