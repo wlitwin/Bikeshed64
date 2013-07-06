e242a6c292a72bacce826a6e6d4eda58dd2e4a13
Tietokone <wtl4112@rit.edu>
2013-07-05 23:39:35 -0400

    Added a limit to the watermark allocator

--- kernel/src/kernel/alloc/alloc.c
+++ kernel/src/kernel/alloc/alloc.c
@@ -6,5 +6,6 @@ WaterMarkAllocator kernel_WaterMark;
 
 void alloc_init()
 {
-	water_mark_init(&kernel_WaterMark, (void*)0xFFFFFFFFFFE00000);
+	// Give it 4MiB of space
+	water_mark_init(&kernel_WaterMark, (void*)0xFFFFFFFFFFE00000, 0x400000);
 }
