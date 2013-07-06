fa6ba5407e6be6fd17271ebd1857e115ba4d2191
Tietokone <wtl4112@rit.edu>
2013-07-05 23:39:14 -0400

    Added another method that will allocate aligned memory

--- kernel/src/kernel/data_structures/watermark.c
+++ kernel/src/kernel/data_structures/watermark.c
@@ -3,15 +3,35 @@
 #include "kernel/panic.h"
 #include "kernel/virt_memory/defs.h"
 
-void water_mark_init(WaterMarkAllocator* wma, const void* location)
+void water_mark_init(WaterMarkAllocator* wma, const void* location, const uint64_t max_size)
 {
 	wma->current_address = (uint64_t)location;
 	wma->page_address = (uint64_t)location;
+	wma->current_size = 0;
+	wma->max_size = max_size;
 }
 
 void* water_mark_alloc(WaterMarkAllocator* wma, const uint64_t size)
 {
-	wma->current_address -= size;
+	return water_mark_alloc_align(wma, 1, size);
+}
+
+void* water_mark_alloc_align(WaterMarkAllocator* wma, const uint64_t alignment, const uint64_t size)
+{
+	// Make sure the alignment is a power of two
+	ASSERT((alignment != 0) && ((alignment & (alignment - 1)) == 0));
+
+	// Figure out how much padding until the next alignment
+	const uint64_t new_address = ALIGN(wma->current_address, alignment);
+	const uint64_t adjusted_size = (new_address - wma->current_address) + size;
+
+	if (wma->current_size + adjusted_size > wma->max_size)
+	{
+		panic("Watermark: Size exceeded!");
+	}
+
+	wma->current_address -= adjusted_size;
+	wma->current_size += adjusted_size;
 	while (wma->current_address < wma->page_address)
 	{
 		const uint64_t location = (wma->page_address - PAGE_LARGE_SIZE);
--- kernel/src/kernel/data_structures/watermark.h
+++ kernel/src/kernel/data_structures/watermark.h
@@ -19,6 +19,8 @@ typedef struct
 {
 	uint64_t page_address;
 	uint64_t current_address;	
+	uint64_t current_size;
+	uint64_t max_size;
 } WaterMarkAllocator;
 
 /* Initialize the watermark object. Doesn't do much.
@@ -29,7 +31,7 @@ typedef struct
  *    wma - The WaterMarkAllocator to initialize
  *    location - The maximum address of the allocator
  */
-void water_mark_init(WaterMarkAllocator* wma, const void* location);
+void water_mark_init(WaterMarkAllocator* wma, const void* location, const uint64_t max_size);
 
 /* Allocates a region of memory.
  *
@@ -42,5 +44,18 @@ void water_mark_init(WaterMarkAllocator* wma, const void* location);
  *    something went wrong
  */
 void* water_mark_alloc(WaterMarkAllocator* wma, const uint64_t size);
+
+/* Allocates a region of memory aligned to a boundary.
+ *
+ * Parameters:
+ *    wma - The WaterMarkAllocator to allocate from
+ *    alignment - The byte boundary to align the memory to
+ *    size - How many bytes to allocate
+ *
+ * Returns:
+ *    A Pointer to the allocated memory, or NULL if 
+ *    something went wrong
+ */
+void* water_mark_alloc_align(WaterMarkAllocator* wma, const uint64_t alignment, const uint64_t size);
 
 #endif
