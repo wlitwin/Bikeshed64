268213e79da048868f31d8b253d05d350643825c
Tietokone <wtl4112@rit.edu>
2013-07-07 15:42:23 -0400

    Changed the random number generator around

--- programs/src/init/rand.c
+++ programs/src/init/rand.c
@@ -1,21 +1,22 @@
 #include "rand.h"
 
-#define DEFAULT_MZ 0x1234567890ABCDEF
-#define DEFAULT_MW 0xC001C0DEC001C0DE
+#define DEFAULT_MZ 0x12345678
+#define DEFAULT_MW 0xC001C0DE
 
-static uint64_t m_z = DEFAULT_MZ;
-static uint64_t m_w = DEFAULT_MW;
+static uint32_t next = 0xC001C0DE;
 
 void seed(uint64_t seed)
 {
-	m_z = DEFAULT_MZ;
-	m_w = seed;
+	next = (uint32_t)seed;
+}
+
+static uint32_t random32()
+{
+	next = next * 1103515245 + 12345;
+	return (uint32_t)(next / 65536) % 32768;
 }
 
 uint64_t rand()
 {
-	m_z = 36969 * (m_z & 0xFFFFFFFF) + (m_z >> 32);
-	m_w = 18000 * (m_w & 0xFFFFFFFF) + (m_w >> 32);
-
-	return (m_z << 32) + m_w;
+	return random32();
 }
